2dec37525d7ad8ab31e1680c8fbfff595c4a62b0
sami suliman <sami.bco@gmail.com>
2019-09-04 01:40:24 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,13 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+class Dancer
+  extend Dance
+  include MetaDancing 
+
+  attr_accessor :name 
+
+  def initialize(name)
+    @name = name
+  end
+
+end
